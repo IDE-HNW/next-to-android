becab2eb3a6fd4de78323585cca37f8acd29b07a
DoHyeon <kdh200533@gmail.com>
2022-09-17 19:02:04 +0900

    Add : Main Fragment
    2022-09-17

--- app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
+++ app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
@@ -2,9 +2,13 @@ package kr.hs.ide.nextto.features.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import kr.hs.ide.nextto.R
+import kr.hs.ide.nextto.databinding.FragmentMainBinding
+import kr.hs.ide.nextto.features.main.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel = MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/kr/hs/ide/nextto/features/main/vm/MainViewModel.kt
@@ -0,0 +1,9 @@
+package kr.hs.ide.nextto.features.main.vm
+
+import androidx.lifecycle.ViewModel
+import kr.hs.ide.nextto.databinding.FragmentMainBinding
+
+class MainViewModel : ViewModel(){
+    private lateinit var FragmentMainBinding
+
+}
