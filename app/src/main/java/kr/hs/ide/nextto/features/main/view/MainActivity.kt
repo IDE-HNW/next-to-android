e2028c200006e205213e1ee1e2169588efc8ac21
최민재 <cmjcmj1230@gmail.com>
2022-09-17 17:28:59 +0900

    Add : Third Party library

--- app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
+++ app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.hs.ide.nextto
+package kr.hs.ide.nextto.features.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.ide.nextto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
