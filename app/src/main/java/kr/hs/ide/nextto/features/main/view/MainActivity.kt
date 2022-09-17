2c12f11d69ffe1019f922770b5f7644983a9f2f6
DoHyeon <kdh200533@gmail.com>
2022-09-17 23:27:43 +0900

    Add : Bottom Nav
    2022-09-17

--- app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
+++ app/src/main/java/kr/hs/ide/nextto/features/main/view/MainActivity.kt
@@ -3,13 +3,29 @@ package kr.hs.ide.nextto.features.main.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import kr.hs.ide.nextto.R
+import kr.hs.ide.nextto.databinding.ActivityMainBinding
 import kr.hs.ide.nextto.databinding.FragmentMainBinding
 import kr.hs.ide.nextto.features.main.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_Container_View) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        binding.bottomNav
+            .setupWithNavController(navController)
+
+        when(binding.bottomNav.selectedItemId){
+        }
     }
 }
--- app/src/main/java/kr/hs/ide/nextto/features/main/view/MainFragment.kt
+++ app/src/main/java/kr/hs/ide/nextto/features/main/view/MainFragment.kt
@@ -50,6 +50,5 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.
     }
 }
