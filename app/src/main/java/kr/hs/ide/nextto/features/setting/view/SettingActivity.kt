d7de97f5a3fdc752f9cb1acafa2d14703cdcb245
최민재 <cmjcmj1230@gmail.com>
2022-09-18 00:54:08 +0900

    Add : Tip item

--- app/src/main/java/kr/hs/ide/nextto/features/setting/view/SettingActivity.kt
+++ app/src/main/java/kr/hs/ide/nextto/features/setting/view/SettingActivity.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kr.hs.ide.nextto.R
